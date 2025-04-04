c3ec365ad8343182ad009660bb971d2a3f950494
Samantha Chow <61718221+sc-chow@users.noreply.github.com>
2025-04-04 13:45:58 -0700

    fixing menutab

--- menutab.js
+++ menutab.js
@@ -1,15 +1,24 @@
-function openTab(evnt,pageName){
-  var i, tabcontent,tablink;
-  tabcontent = document.getElementbyClassName("tabcontent");
-  for(i = 0; i<tabcontent.length;i++){
+function openTab(evt, pageName) {
+  var i, tabcontent, tablink;
+
+  // Hide all tab content
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablink = document.getElementbyClassName("tablink");
-  for(i = 0; i<tablink.length;i++){
-    tablink[i].className = tablink[i].className.replace("active","");
+
+  // Remove "active" from all buttons
+  tablink = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablink.length; i++) {
+    tablink[i].classList.remove("active");
   }
-  
+
+  // Show the selected tab and mark the button as active
   document.getElementById(pageName).style.display = "block";
-  
-  evt.currentTarget.className += " active";
+  evt.currentTarget.classList.add("active");
 }
+
+// Automatically open the first tab on page load
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".tablink").click();
+});
